a084405e7ccc4aaf05a136a9207acc16d7bf193f
TheSoulounias <142552158+TheSoulounias@users.noreply.github.com>
2023-12-30 23:49:30 -0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,66 +2,68 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'TheSoulounias', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['TheSoulounias'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    facebook: '',
-    instagram: '',
-    youtube: '', // example: 'pewdiepie'
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    skype: '',
-    telegram: '',
-    website: 'https://arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
+    linkedin: 'themelis-soulounias',
+    //twitter: 'arif_szn',
+   // mastodon: 'arifszn@mastodon.social',
+   // facebook: '',
+   // instagram: '',
+   // youtube: '', // example: 'pewdiepie'
+   // dribbble: '',
+   // behance: '',
+   // medium: 'arifszn',
+   // dev: 'arifszn',
+   // stackoverflow: '', // example: '1/jeff-atwood'
+   // skype: '',
+   // telegram: '',
+   // website: 'https://arifszn.com',
+   // phone: '',
+    email: 'TheSoulounias@gmail.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+'', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
+    'Excel',
+    'SQL',
+    'Python',
+    'Linux',
+    'Microsoft PowerApps',
+    'R Programming',
     'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Pandas',
+    
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'Certified Ceramics',
+      position: 'Business and Marketing Analyst',
+      from: 'August 2023',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://certifiedceramics.com',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Skanska',
+      position: 'EHS Manager',
+      from: 'March 2023',
+      to: 'October 2023',
+      companyLink: 'https://skanska.com',
+    },
+    {
+      company: 'Delaware River Port Authority',
+      position: 'Safety Specialist',
+      from: 'September 2019',
+      to: 'March 2023',
+      companyLink: 'https://drpa.org',
     },
   ],
   /* certifications: [
@@ -74,42 +76,42 @@ const config = {
   ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'College of Arts and Sciences, University of South Florida',
+      degree: 'Bachelor of Science, Chemistry',
+    //  from: '2015',
+   //   to: '2019',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'College of Arts and Sciences, Saint Petersburg College',
+      degree: 'Associate of Science, Biomedical',
+    //  from: '2012',
+    //  to: '2014',
     },
   ],
 
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
-      title: 'Project Name',
+      title: 'NBA Stat Scrape and Analysis',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'Ongoing project to scrape and analyze leader stats from the NBA website.',
+      imageUrl: 'https://github.com/TheSoulounias/TheSoulounias/blob/5a02b1fc4bc2ac386a305397a98f7e11ac9ee373/firefox_Qzp1xxVxvK.png?raw=true',
+      link: 'https://www.kaggle.com/code/themelissoulounias/nba-stat-scrape-and-analysis',
     },
     {
-      title: 'Project Name',
+      title: 'Revenue Map',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'A short tutorial on placing data on a map. To help decision makers visualize areas for ad targeting',
+      imageUrl: 'https://github.com/TheSoulounias/TheSoulounias/blob/ab380cb07338a415db36793373d996f87c43e6b5/firefox_H55SYMnpi0.png?raw=true',
+      link: 'https://www.kaggle.com/themelissoulounias/revenue-map',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
-  },
+//  blog: {
+ //   source: 'dev', // medium | dev
+//    username: 'arifszn', // to hide blog section, keep it empty
+//    limit: 2, // How many posts to display. Max is 10.
+//  },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
@@ -119,7 +121,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'wireframe',
+    defaultTheme: 'business',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -179,11 +181,11 @@ const config = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+ // footer: `Made with <a 
+ //     class="text-primary" href="https://github.com/arifszn/gitprofile"
+ //     target="_blank"
+ //     rel="noreferrer"
+ //   >GitProfile</a> and â¤ï¸`,
 };
 
 export default config;
