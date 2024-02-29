3a96fab9b32f852ebd8c27c57cb366559d006376
Themelis Soulounias <142552158+TheSoulounias@users.noreply.github.com>
2024-02-29 13:57:32 -0500

    Update gitprofile.config.js
    Remove odin projects from appearing on portfolio page.

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -7,7 +7,7 @@ const config = {
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: ['TheSoulounias','git_test','odin-recipes'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['TheSoulounias','git_test','odin-recipes','javascript-exercises','rock-paper-scissors','odin-landing-page-project','css-exercises'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
